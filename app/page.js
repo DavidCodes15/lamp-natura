19e886e1992e4740e670501810fad6dfba404c29
david magaldadze <davidcodes@davids-MacBook-Air.local>
2023-10-05 22:20:11 -0700

    started a website with home page

--- app/page.js
+++ app/page.js
@@ -1,113 +1,205 @@
-import Image from 'next/image'
-
+import { LucideBackpack, LucideKey } from "lucide-react";
+import "./globals.css";
+let amount = 0;
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <>
+      <header>
+        <nav className="flex justify-between items-center px-12 py-4">
+          <ul>
+            <li className="text-2xl text-[#ff9a00]">ნათურა</li>
+          </ul>
+          <ul className="flex justify-center space-x-12 items-center text-lg">
+            <li className="cursor-pointer hover:text-[#ff9a00] duration-150">
+              პროდუქტი
+            </li>
+            <li className="cursor-pointer hover:text-[#ff9a00] duration-150">
+              გუნდი
+            </li>
+            <li className="cursor-pointer hover:text-[#ff9a00] duration-150">
+              ჩვენს შესახებ
+            </li>
+          </ul>
+          <ul>
+            <button className="bg-[#ff9a00] rounded px-4 py-2 text-white text-md flex justify-center items-center space-x-[5px]">
+              <span>
+                <LucideBackpack />
+              </span>
+              <span>{amount}</span>
+            </button>
+          </ul>
+        </nav>
+        <div className="w-[90%] h-[2px] rounded-full bg-[#ff9a00] mx-auto mt-[10px]"></div>
+      </header>
+      <main>
+        <section id="hero" className="container mx-auto mt-24">
+          <div className="flex flex-col justify-center items-center space-y-6">
+            <div className="flex justify-center items-center space-x-2">
+              <h1 className="text-2xl custom-heading-class">
+                <span className="animate-color-change">
+                  მომავალი ღონისძიებები
+                </span>
+              </h1>
+              <span>
+                <img
+                  className="w-[40px] h-[40px]"
+                  src="/images/icons/light-bulbik.svg"
+                  alt="light bulb"
+                />
+              </span>
+            </div>
+            <div className="w-full flex justify-center items-center">
+              <img
+                className="w-[80%] h-[500px] rounded-xl"
+                src="/images/events/event-placeholder-second.jpeg"
+                alt="event placeholder"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="container mx-auto mt-24">
+          <div className="flex justify-center items-center flex-col space-y-12">
+            <div className="flex justify-center items-center text-3xl">
+              <h2>პროდუქტები</h2>
+            </div>
+            <div className="grid grid-cols-3 gap-9">
+              <div className="relative border-[0.1px] border-solid border-black rounded-xl px-2 py-2 flex flex-col justify-center items-start space-y-4">
+                <div>
+                  <img
+                    className="rounded-xl"
+                    src="/images/products/natura.png"
+                    alt="natura product"
+                  />
+                </div>
+                <div className="flex justify-center items-start flex-col space-y-2 px-2">
+                  <span className="text-xl">ბურგერი შავი ტრუფელით</span>
+                  <span>
+                    <p className="max-w-[80%] text-[#c29553]">
+                    პური, საქონლის ხორცი, კარამელიზირებული ხახვი, კეჩუნეზი, ყველის სოუსი, შავი ტრუფელი
+                    </p>
+                  </span>
+                  <span className="flex justify-center items-center space-x-2">
+                    <span className="text-lg">13.44 $</span>
+                    <button className="bg-[#ff9a00] rounded px-4 py-2">
+                      <LucideBackpack
+                        color="white"
+                        className="w-[20px] h-[20px]"
+                      />
+                    </button>
+                  </span>
+                  <span className="flex justify-center items-center space-x-2">
+                    <span className="text-md">ნახე როგორ მზადდება</span>
+                    <button className="bg-[#ff9a00] rounded px-4 py-2">
+                      <LucideKey color="white" className="w-[20px] h-[20px]" />
+                    </button>
+                  </span>
+                </div>
+                <div className="absolute top-0 right-2 bg-[#f8ac3c] rounded px-4 py-2 text-white">
+                  <span>პოპულარული</span>
+                </div>
+              </div>
+              <div className="border-[0.1px] border-solid border-black rounded-xl px-2 py-2 flex flex-col justify-center items-start space-y-4">
+                <div>
+                  <img
+                    className="rounded-xl"
+                    src="/images/products/product3.png"
+                    alt="natura product"
+                  />
+                </div>
+                <div className="flex justify-center items-start flex-col space-y-2 px-2">
+                  <span className="text-xl">ძაან ბურგერი</span>
+                  <span>
+                    <p className="max-w-[80%] text-[#c29553]">
+                    პური,საქონლის ხორცი,საფირმო სოუსი,აისბერგი,ყველი ჩედარი,კომბოსტოს მარინადი
+                    </p>
+                  </span>
+                  <span className="flex justify-center items-center space-x-2">
+                    <span className="text-lg">11.44 $</span>
+                    <button className="bg-[#ff9a00] rounded px-4 py-2">
+                      <LucideBackpack
+                        color="white"
+                        className="w-[20px] h-[20px]"
+                      />
+                    </button>
+                  </span>
+                  <span className="flex justify-center items-center space-x-2">
+                    <span className="text-md">ნახე როგორ მზადდება</span>
+                    <button className="bg-[#ff9a00] rounded px-4 py-2">
+                      <LucideKey color="white" className="w-[20px] h-[20px]" />
+                    </button>
+                  </span>
+                </div>
+              </div>
+              <div className="border-[0.1px] border-solid border-black rounded-xl px-2 py-2 flex flex-col justify-center items-start space-y-4">
+                <div>
+                  <img
+                    className="rounded-xl"
+                    src="/images/products/product2.png"
+                    alt="natura product"
+                  />
+                </div>
+                <div className="flex justify-center items-start flex-col space-y-2 px-2">
+                  <span className="text-xl">ცივი ჩაი</span>
+                  <span>
+                    <p className="max-w-[80%] text-[#c29553]">
+                      ცივი ყავა ნაყინით დაბალკალორიული ცივი ყავა ნაყინით
+                    </p>
+                  </span>
+                  <span className="flex justify-center items-center space-x-2">
+                    <span className="text-lg">10.44 $</span>
+                    <button className="bg-[#ff9a00] rounded px-4 py-2">
+                      <LucideBackpack
+                        color="white"
+                        className="w-[20px] h-[20px]"
+                      />
+                    </button>
+                  </span>
+                  <span className="flex justify-center items-center space-x-2">
+                    <span className="text-md">ნახე როგორ მზადდება</span>
+                    <button className="bg-[#ff9a00] rounded px-4 py-2">
+                      <LucideKey color="white" className="w-[20px] h-[20px]" />
+                    </button>
+                  </span>
+                </div>
+              </div>
+              <div className="border-[0.1px] border-solid border-black rounded-xl px-2 py-2 flex flex-col justify-center items-start space-y-4">
+                <div>
+                  <img
+                    className="rounded-xl"
+                    src="/images/products/product1.png"
+                    alt="natura product"
+                  />
+                </div>
+                <div className="flex justify-center items-start flex-col space-y-2 px-2">
+                  <span className="text-xl">წვენი</span>
+                  <span>
+                    <p className="max-w-[80%] text-[#c29553]">
+                      ფრეში სტაფილო და გრეიფრუტი ფორთოხალი მწვანე ვაშლი მწვანე
+                      ვაშლი და სტაფილო
+                    </p>
+                  </span>
+                  <span className="flex justify-center items-center space-x-2">
+                    <span className="text-lg">9.44 $</span>
+                    <button className="bg-[#ff9a00] rounded px-4 py-2">
+                      <LucideBackpack
+                        color="white"
+                        className="w-[20px] h-[20px]"
+                      />
+                    </button>
+                  </span>
+                  <span className="flex justify-center items-center space-x-2">
+                    <span className="text-md">ნახე როგორ მზადდება</span>
+                    <button className="bg-[#ff9a00] rounded px-4 py-2">
+                      <LucideKey color="white" className="w-[20px] h-[20px]" />
+                    </button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer></footer>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'color-change': 'colorChange 4s infinite', // Define a custom animation named 'color-change'
+      },
+      keyframes: {
+        colorChange: {
+          '0%, 100%': { color: 'black', transform: 'scale(1)' },
+          '20%': { color: '#ff9a00', transform: 'scale(1.2)' },
+          '40%': { color: '#ff9a00', transform: 'translateY(-5px)' },
+          '60%': { color: '#ff9a00', transform: 'scale(1.1)' },
+          '80%': { color: '#ff9a00', transform: 'translateY(2px)' },
+        },
+      },
     },
   },
   plugins: [],
